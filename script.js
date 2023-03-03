e90565bf1783c4913672bc0402d813388f99f36e
Danil Brudanin <brudankovko@gmail.com>
2023-03-03 20:49:21 +0300

    v.0.2.0

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const butSymbols = document.querySelectorAll('.symbols')
 
 butSymbols.forEach(button => {
     button.addEventListener('click', (a) => {
-        if (a.target.style.borderColor !=='palevioletred') {
+        if (a.target.style.borderColor !=='palevioletred' && a.target.style.backgroundColor !== '#ee9bb7' ) {
             a.target.style.borderColor = 'palevioletred';
+            a.target.style.backgroundColor = '#ee9bb7'
         } else {
-            a.target.style.borderColor = 'black'
+            a.target.style.borderColor = '#949494'
+            a.target.style.backgroundColor = 'white'
         }
     })
 });
