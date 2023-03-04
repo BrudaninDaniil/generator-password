a33b5586a989f8fcd68889747bbfdebb6a44b16f
Danil Brudanin <brudankovko@gmail.com>
2023-03-04 14:14:55 +0300

    v.1.0.0

--- script.js
+++ script.js
@@ -21,74 +21,42 @@ const butNumbers = document.querySelectorAll('.symbols')[2]
 const butSymbols = document.querySelectorAll('.symbols')[3]
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+function checkButton(button) {
+    return button.style.borderColor == 'palevioletred' ? true : false ;
+}
+
 function getRandomPassword() {
     let numbers = '0123456789';
     let letters = 'abcdefghijklmnopqrstuvwxyz'
     let upLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     let $ymbols = '!@#$%^&-_+='
+    let allowedSymbols = ''
     let randomPassword = ''
-    let randomPassword2 = ''
-    let randomletters, randomUpLetters, randomNumbers, random$ymbols
 
+    if (checkButton(butSmallLetters)) {
+        allowedSymbols += letters ;
+    }
+    
+    if (checkButton(butUpLetters)) {
+        allowedSymbols += upLetters ;
+    }
+
+    if (checkButton(butNumbers)) {
+        allowedSymbols += numbers ;
+    }
+
+    if (checkButton(butSymbols)) {
+        allowedSymbols += $ymbols ;
+    }
+    
     for (let i = 0; i < inputLenght.value; i++) {
-        if (butSmallLetters.style.borderColor == 'palevioletred' 
-        && butUpLetters.style.borderColor !== 'palevioletred' 
-        && butNumbers.style.borderColor !== 'palevioletred' 
-        && butSymbols.style.borderColor !== 'palevioletred' ) {
-            randomPassword = randomPassword + letters[Math.floor(Math.random() * 26)];
-        } 
-        if (butUpLetters.style.borderColor == 'palevioletred' 
-        && butSmallLetters.style.borderColor !== 'palevioletred' 
-        && butNumbers.style.borderColor !== 'palevioletred' 
-        && butSymbols.style.borderColor !== 'palevioletred' ) {
-            randomPassword += upLetters[Math.floor(Math.random() * 26)];
-        }
-        if (butNumbers.style.borderColor == 'palevioletred' 
-        && butUpLetters.style.borderColor !== 'palevioletred' 
-        && butSmallLetters.style.borderColor !== 'palevioletred' 
-        && butSymbols.style.borderColor !== 'palevioletred' ) {
-            randomPassword += numbers[Math.floor(Math.random() * 10)];
-        }
-        if (butSymbols.style.borderColor == 'palevioletred' 
-        && butUpLetters.style.borderColor !== 'palevioletred' 
-        && butSmallLetters.style.borderColor !== 'palevioletred' 
-        && butNumbers.style.borderColor !== 'palevioletred' ) {
-            randomPassword += $ymbols[Math.floor(Math.random() * 11)];
-        } 
-        if (butSmallLetters.style.borderColor == 'palevioletred' 
-        && butUpLetters.style.borderColor == 'palevioletred' 
-        && butNumbers.style.borderColor !== 'palevioletred' 
-        && butSymbols.style.borderColor !== 'palevioletred' ) {
-            if (i % 2 == 0) {
-                i < inputLenght / 2;
-                randomPassword += upLetters[Math.floor(Math.random() * 26)] + letters[Math.floor(Math.random() * 26)];
-            }
-        }
-        if (butSmallLetters.style.borderColor == 'palevioletred' 
-        && butUpLetters.style.borderColor !== 'palevioletred' 
-        && butNumbers.style.borderColor == 'palevioletred' 
-        && butSymbols.style.borderColor !== 'palevioletred' ) {
-            if (i % 2 == 0) {
-                i < inputLenght / 2;
-                randomPassword += numbers[Math.floor(Math.random() * 10)] + letters[Math.floor(Math.random() * 26)];
-            } 
-            console.log(randomPassword);
-        }
-        if (butSmallLetters.style.borderColor == 'palevioletred' 
-        && butUpLetters.style.borderColor == 'palevioletred' 
-        && butNumbers.style.borderColor == 'palevioletred' 
-        && butSymbols.style.borderColor !== 'palevioletred' ) {
-            randomPassword += numbers[Math.floor(Math.random() * 10)] + letters[Math.floor(Math.random() * 26)] + upLetters[Math.floor(Math.random() * 26)];
-        }
+        randomPassword += allowedSymbols[getRandomInt(0, allowedSymbols.length-1)]
 
+    }
 
-    }    
     return randomPassword ;
 }    
-
 
 butGenerate.addEventListener('click', () => {
     inputPass.value = getRandomPassword()
 })
-
-
